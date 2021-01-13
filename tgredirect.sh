7c4c0ea2ae892e93d3490e33971e6408d9c6934c
CC <cc@CPROM>
2021-01-13 11:55:34 +0100

    +ReadStdIn \!FetchOnlyImportant

--- tgredirect.sh
+++ tgredirect.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-curl --request POST --url "$1" --data "$2"
+importantlines=""
+if [ -p /dev/stdin ]; then
+	while IFS= read -r line; do
+		if [[ $line =~ .*Verif.* ]]; then
+			importantlines="|$importantlines$line"
+		fi
+	done
+fi
+
+curl --request POST --url "$1" --data-raw "$2$importantlines"
